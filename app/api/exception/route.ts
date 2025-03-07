22f9eecb12e6af53e56ce1a308d77c7a288e1aea
Marko Mänty <mtmanty@gmail.com>
2025-03-07 17:53:16 +0200

    Remove caching

--- app/api/exception/route.ts
+++ app/api/exception/route.ts
@@ -2,13 +2,18 @@ import { NextResponse } from "next/server"
 
 export async function GET() {
     try {
-        const response = await fetch("http://localhost:8080/exception", {
+        const response = await fetch(`${process.env.API_HOST}/exception`, {
             method: "GET",
             headers: new Headers({ 'content-type': 'application/json' }),
+            cache: "no-store",
         })
 
         const data = await response.json()
-        return NextResponse.json(data)
+        return NextResponse.json(data, {
+            headers: {
+                "Cache-Control": "no-store, max-age=0",
+            },
+        });
     } catch (error) {
         console.error("Error:", error)
         return NextResponse.json({ error: "Failed to fetch from external service" }, { status: 500 })
--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -2,17 +2,27 @@ import { NextResponse } from "next/server"
 
 export async function GET() {
     try {
-        const response = await fetch("http://localhost:8080/hello", {
+        var url = `${process.env.API_HOST}/hello`;
+        console.log("URL: ", url);
+
+        const response = await fetch(url, {
             method: "GET",
             headers: new Headers({ 'content-type': 'application/json' }),
+            cache: "no-store",
         })
+
+        console.log("Response: ", response);
 
         if (!response.ok) {
             throw new Error("External service response was not ok")
         }
 
         const data = await response.json()
-        return NextResponse.json(data)
+        return NextResponse.json(data, {
+            headers: {
+                "Cache-Control": "no-store, max-age=0",
+            },
+        });
     } catch (error) {
         console.error("Error:", error)
         return NextResponse.json({ error: "Failed to fetch from external service" }, { status: 500 })
